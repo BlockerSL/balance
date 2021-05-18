b57405e4646db7f408ac863a40db37c45a8719c5
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-18 17:58:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-function sahand() {  
+function sahand()
+{
   var Tag = document.getElementById("Tag").value;
+  if (Tag == "H2(g)+Cl2(g)") {
 
-  if (Tag=="H")
-  {
-    window.location.href="sahand/index.html";
-
+  window.open('https://balance-1.netlify.app', '_blank');
   }else{
-    alert("incorrect")
-  }
+alert("Incorrect");
+}
 }
