399f91c0ea2b05bb1b342be6e18e6e6551d3d08b
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-18 17:32:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function sahand() {  
+  var Tag = document.getElementById("Tag").value;
+
+  if (Tag == "H" )
+  {
+    window.location.href="sahand/index.html";
+
+  }else{
+    alert("incorrect")
+  }
+}
