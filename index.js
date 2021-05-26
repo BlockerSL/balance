d6bc5cfdb9b46834b9c6e7f5d5cc205e2066ed5c
BlockerSL <82181981+BlockerSL@users.noreply.github.com>
2021-05-26 17:14:11 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function sahand()
+{
+  var Tag = document.getElementById("Tag").value;
+  if (Tag == "H2(g)+Cl2(g)") {
+
+  window.open('https://balance-1.netlify.app', '_blank');
+  }else{
+alert("Incorrect");
+}
+}
